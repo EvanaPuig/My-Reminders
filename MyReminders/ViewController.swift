d23b407dbe49b03731a3707a9040e873b4e3d091
evana-puig <evana.puig@bairesdev.com>
2019-08-20 18:19:56 -0500

    Initial Commit

---
+++ MyReminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyReminders
+//
+//  Created by Evana Margain on 8/20/19.
+//  Copyright © 2019 Evana Margain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
