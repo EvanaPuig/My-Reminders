a6c8a2166b4edd88a737a52ee6053750db921049
evana-puig <evana.puig@bairesdev.com>
2019-08-30 19:54:44 -0500

    Adds a new screen for adding todo items

---
+++ MyReminders/Controller/AddItemTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddItemTableViewController.swift
+//  MyReminders
+//
+//  Created by Evana Margain on 8/30/19.
+//  Copyright © 2019 Evana Margain. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+}
