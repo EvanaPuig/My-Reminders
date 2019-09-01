245fd18a0a25d2bacdb0391dc4993b04f302cff1
evana-puig <evana.puig@bairesdev.com>
2019-09-01 13:30:00 -0500

    Controls the textfield so that only if the user has typed something the add button is enabled, implements uitextfielddelegate needed methods

--- MyReminders/Controller/AddItemTableViewController.swift
+++ MyReminders/Controller/AddItemTableViewController.swift
@@ -11,10 +11,23 @@ import UIKit
 class AddItemTableViewController: UITableViewController {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.largeTitleDisplayMode = .never
+        
+        textField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
     
     @IBAction func cancel(_ sender: Any) {
@@ -23,5 +36,30 @@ class AddItemTableViewController: UITableViewController {
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+        print("Contents of the textfield \(String(describing: textField.text))")
+    }
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+            let stringRange = Range(range, in: oldText) else {
+                return false
+        }
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            addBarButton.isEnabled = false
+        } else {
+            addBarButton.isEnabled = true
+        }
+        
+        return true
     }
 }
